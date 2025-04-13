f81941074652bc1446f9fd409fc42e55b74580c0
Daniel Deaconescu <daniel.deaconescu23@gmail.com>
2025-04-14 01:20:35 +0300

    further changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function userScroll() {
-  const navbar = document.querySelector('.navbar');
-  const toTopBtn = document.querySelector('#to-top');
+  const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector("#to-top");
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
-      navbar.classList.add('navbar-sticky');
-      toTopBtn.classList.add('show');
+      navbar.classList.add("navbar-sticky");
+      toTopBtn.classList.add("show");
     } else {
-      navbar.classList.remove('navbar-sticky');
-      toTopBtn.classList.remove('show');
+      navbar.classList.remove("navbar-sticky");
+      toTopBtn.classList.remove("show");
     }
   });
 }
@@ -19,13 +19,13 @@ function scrollToTop() {
 }
 
 function incrementStats() {
-  const counters = document.querySelectorAll('.counter');
+  const counters = document.querySelectorAll(".counter");
 
   counters.forEach((counter) => {
     counter.innerText = 0;
 
     const updateCounter = () => {
-      const target = +counter.getAttribute('data-target');
+      const target = +counter.getAttribute("data-target");
       const c = +counter.innerText;
 
       const increment = target / 200;
@@ -43,6 +43,31 @@ function incrementStats() {
 }
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', userScroll);
-document.addEventListener('DOMContentLoaded', incrementStats);
-document.querySelector('#to-top').addEventListener('click', scrollToTop);
+document.addEventListener("DOMContentLoaded", userScroll);
+document.addEventListener("DOMContentLoaded", incrementStats);
+document.querySelector("#to-top").addEventListener("click", scrollToTop);
+
+// Video Modal
+const videoBtn = document.querySelector(".video-btn");
+const videoModal = document.querySelector("#videoModal");
+const video = document.querySelector("#video");
+let videoSrc;
+
+if (videoBtn !== null) {
+  videoBtn.addEventListener("click", () => {
+    videoSrc = videoBtn.getAttribute("data-bs-src");
+  });
+}
+
+if (videoModal !== null) {
+  videoModal.addEventListener("shown.bs.modal", () => {
+    video.setAttribute(
+      "src",
+      `${videoSrc}?autoplay=1&modestbranding=1&showInfo=0`
+    );
+  });
+
+  videoModal.addEventListener("hide.bs.modal", () => {
+    video.setAttribute("src", "");
+  });
+}
